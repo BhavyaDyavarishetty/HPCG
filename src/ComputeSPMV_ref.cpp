5dd8d13e1207b699b24aa840578db66bdf458a8a
Steven R. Brandt <sbrandt@cct.lsu.edu>
2014-05-22 09:24:29 -0500

    parallelized another loop with futures

--- src/ComputeSPMV_ref.cpp
+++ src/ComputeSPMV_ref.cpp
@@ -20,6 +20,10 @@
 
 #include "ComputeSPMV_ref.hpp"
 
+#ifndef HPCG_NOHPX
+#include "parallel_for.hpp"
+#endif
+
 #ifndef HPCG_NOMPI
 #include "ExchangeHalo.hpp"
 #endif
@@ -55,6 +59,27 @@ int ComputeSPMV_ref( const SparseMatrix & A, Vector & x, Vector & y) {
   const double * const xv = x.values;
   double * const yv = y.values;
   const local_int_t nrow = A.localNumberOfRows;
+#ifndef HPCG_NOHPX
+  auto f = [&A,yv,xv](int i){
+    double sum = 0.0;
+    const double * const cur_vals = A.matrixValues[i];
+    const local_int_t * const cur_inds = A.mtxIndL[i];
+    const int cur_nnz = A.nonzerosInRow[i];
+
+    for (int j=0; j< cur_nnz; j++)
+      sum += cur_vals[j]*xv[cur_inds[j]];
+    yv[i] = sum;
+    return 0;
+  };
+  auto r = [](int,int){
+    return 0;
+  };
+  parallel_sum<decltype(f),local_int_t,decltype(r)>(
+    hpx_nprocs,hpx_chunksize,
+    f,
+    0,nrow,
+    r,0);
+#else
 #ifndef HPCG_NOOPENMP
   #pragma omp parallel for
 #endif
@@ -68,5 +93,6 @@ int ComputeSPMV_ref( const SparseMatrix & A, Vector & x, Vector & y) {
       sum += cur_vals[j]*xv[cur_inds[j]];
     yv[i] = sum;
   }
+#endif
   return(0);
 }
